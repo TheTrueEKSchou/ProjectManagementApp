445daad832bdaf39ece0fec6871a656d1fb034f8
Emil Konggaard Schou <emil587a@edu.campusvejle.dk>
2018-09-17 11:09:34 +0200

    More changes

---
+++ ProjectManagementApp.EF/Validate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProjectManagementApp.EF
+{
+    static class Validate
+    {
+        public static bool IsPersonNameValid(string s)
+        {
+            Regex regex = new Regex("^[A-Z][a-z][A-Za-z]+$");
+            if (regex.IsMatch(s))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsEntityNameValid(string s)
+        {
+            Regex regex = new Regex("^[A-Z][A-Za-z0-9]{0-49}$");
+            if (regex.IsMatch(s))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- ProjectManagementApp.Gui/ProjectUserControl.xaml.cs
+++ ProjectManagementApp.Gui/ProjectUserControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectManagementApp.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +21,59 @@ namespace ProjectManagementApp.Gui
     /// </summary>
     public partial class ProjectUserControl : UserControl
     {
+        protected Model model;
+        protected Project selectedProject;
         public ProjectUserControl()
         {
             InitializeComponent();
+            model = new Model();
+            DataGrid_Projects.ItemsSource = model.Projects.ToList();
+        }
+
+        private void Button_SaveNewProject_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_UpdateProject_Click(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void Button_RemoveProject_Click(object sender, RoutedEventArgs e)
         {
-
+            if(selectedProject != null)
+            {
+                Project project = model.Projects.Find(selectedProject.Id);
+                model.Projects.Remove(project);
+                model.SaveChanges();
+                ClearTextBoxes();
+                DataGrid_Projects.ItemsSource = model.Projects.ToList();
+            }
         }
+
+        private void DataGrid_Projects_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedProject = DataGrid_Projects.SelectedItem as Project;
+            if(selectedProject != null)
+            {
+                TextBox_Name.Text = selectedProject.Name;
+                TextBox_Description.Text = selectedProject.Description;
+                DatePicker_StartDate.Text = selectedProject.StartDate.ToString();
+                DatePicker_EndDate.Text = selectedProject.EndDate.ToString();
+                TextBox_BudgetLimit.Text = selectedProject.BudgetLimit.ToString();
+            }
+        }
+
+        private void ClearTextBoxes()
+        {
+            TextBox_Name.Text = "";
+            TextBox_Description.Text = "";
+            DatePicker_StartDate.Text = "";
+            DatePicker_EndDate.Text = "";
+            TextBox_BudgetLimit.Text = "";
+        }
+
+        
     }
 }
